1e9cf50ed0574e9cd02bc942d604fa28cfc2a29d
Radek Molenda <radek.molenda@gmail.com>
2011-08-03 21:59:41 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FayeResque::Application.config.secret_token = '6eec3a05dd462066e8986f762df58dad0dc6cff07309fe4bd2f9087842a89dc62dbb1f2d37ce257cd5737a508ae00521c77388480a0ae848999ce5cfcbdf9ba6'
